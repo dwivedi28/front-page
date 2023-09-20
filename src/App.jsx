a377975029a932498a4bfea5b401521b656f6180
dwivedi28 <142082163+dwivedi28@users.noreply.github.com>
2023-09-21 02:35:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React, { useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Sphere } from "@react-three/drei";
-import{pointsHandsInfinity} from"./utils";
-
+import { pointsInner, pointsOuter } from "./utils";
 
 const ThreadsLanding = () => {
   return (
@@ -11,12 +10,12 @@ const ThreadsLanding = () => {
         camera={{
           position: [10, -7.5, -5],
         }}
-        className="bg-[#101010]" style={{ height: "100vh", width:2000 }}
+        className="bg-[#101010]" style={{ height: "100vh", width:1550}}
       >
         <OrbitControls maxDistance={20} minDistance={10} />
         <directionalLight />
         <pointLight position={[-30, 0, -30]} power={10.0} />
-        <PointHandsInfinity /> 
+        <PointCircle />
       </Canvas>
       <h1 className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-neutral-200 font-medium text-5xl pointer-events-none">
         Sarthi
@@ -25,20 +24,22 @@ const ThreadsLanding = () => {
   );
 };
 
-const PointHandsInfinity = () => {
+const PointCircle = () => {
   const ref = useRef();
   useFrame(({ clock }) => {
-    ref.current.rotation.z = clock.getElapsedTime() * 0.03;
+    ref.current.rotation.z = clock.getElapsedTime() * 0.05; // Fixed typo here
   });
   return (
     <group ref={ref}>
-      {pointsHandsInfinity.map((point) => (
+      {pointsInner.map((point) => (
+        <Point key={point.idx} position={point.position} color={point.color} />
+      ))}
+      {pointsOuter.map((point) => (
         <Point key={point.idx} position={point.position} color={point.color} />
       ))}
     </group>
   );
 };
-
 const Point = ({ position, color }) => {
   return (
     <Sphere position={position} args={[0.1, 10, 10]}>
